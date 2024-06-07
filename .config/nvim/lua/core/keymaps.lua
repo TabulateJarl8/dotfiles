a81977d38e0ad0fd68959c336ccf36330c03faee
TabulateJarl8 <tabulatejarl8@gmail.com>
2024-06-06 23:49:15 -0400

    add more keymaps for buffers

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -16,3 +16,8 @@ vim.api.nvim_set_keymap('n', '<LEFT>', '<C-w>h', { noremap = true, silent = true
 vim.api.nvim_set_keymap('n', '<DOWN>', '<C-w>j', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<UP>', '<C-w>k', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<RIGHT>', '<C-w>l', { noremap = true, silent = true })
+-- Remap ctrl+hjkl to buffer movement as well
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
