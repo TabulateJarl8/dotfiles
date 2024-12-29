c5c5124b7d245c5461d7093736cc72e6447711e5
TabulateJarl8 <tabulatejarl8@gmail.com>
2024-12-28 23:13:36 -0500

    add justMyCode=false to python DAP

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,11 +1,6 @@
 -- This file contains configuration for plugins related to debugging
 return {
 	-- debug adapter protocol for debugging
-	-- <<<<<<< HEAD (Current changes)
-	-- print("nuzzy")
-	-- =======
-	-- print("fuzzy")
-	-- >>>>>>> master (Incoming changes)
 	{
 		"mfussenegger/nvim-dap",
 		dependencies = { "williamboman/mason.nvim" },
@@ -25,7 +20,30 @@ return {
 		opts = {
 			ensure_installed = { "codelldb", "python" },
 			automatic_installation = true,
-			handlers = {},
+			handlers = {
+				function(config)
+					require("mason-nvim-dap").default_setup(config)
+				end,
+				python = function(config)
+					local venv_path = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
+					config.configurations = {
+						{
+							-- The first three options are required by nvim-dap
+							type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+							request = "launch",
+							name = "Python: Launch file",
+							program = "${file}", -- This configuration will launch the current file if used.
+							-- venv on Windows uses Scripts instead of bin
+							pythonPath = venv_path
+									and ((vim.fn.has("win32") == 1 and venv_path .. "/Scripts/python") or venv_path .. "/bin/python")
+								or nil,
+							console = "integratedTerminal",
+							justMyCode = false,
+						},
+					}
+					require("mason-nvim-dap").default_setup(config)
+				end,
+			},
 		},
 	},
 	-- show hints of variable values as virtual text while debugging
