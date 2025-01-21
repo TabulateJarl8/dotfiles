3903118b3495d3ddcb264b097885e36ba77cabee
TabulateJarl8 <tabulatejarl8@gmail.com>
2025-01-21 13:37:39 -0500

    remove old stuff from vim config

--- .config/nvim/lua/core/config.lua
+++ .config/nvim/lua/core/config.lua
@@ -1,16 +1,3 @@
--- Delete whitespace autocmd
--- vim.cmd([[
--- augroup DeleteWhitespace
---     autocmd!
---     autocmd BufWritePre * let currPos = getpos(".")
---     autocmd BufWritePre * %s/\s\+$//e
---     autocmd BufWritePre * %s/\n\+\%$//e
---     autocmd BufWritePre *.[ch] % !astyle -A10s4cxC100S
---     autocmd BufWritePre *.[ch] %s/\%$/\r/e
---     autocmd BufWritePre * call cursor(currPos[1], currPos[2])
--- augroup END
--- ]])
-
 -- Buffer behavior
 vim.o.relativenumber = true
 vim.o.number = true
