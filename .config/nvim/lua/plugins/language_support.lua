e12ce17010763576e326caf1b60ff83268c559ef
TabulateJarl8 <tabulatejarl8@gmail.com>
2026-04-13 01:35:03 -0400

    disable vimtex regex highlighting

--- .config/nvim/lua/plugins/language_support.lua
+++ .config/nvim/lua/plugins/language_support.lua
@@ -50,6 +50,7 @@ return {
 			vim.g.vimtex_view_general_viewer = "okular"
 			vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
 			vim.g.tex_flavor = "latex"
+			vim.g.vimtex_syntax_enabled = 0
 		end,
 	},
 	-- Hugo html template support
